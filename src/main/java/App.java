38cf98e15725f12a9a525c095fb69a9df0c85604
Leah H <harrichl@gmail.com>
2021-04-07 23:36:30 -0400

    connect mariadb java connector

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,25 @@
 import io.javalin.Javalin;
 
+import java.sql.*;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Javalin app = Javalin.create().start(7000);
         app.get("/", ctx -> ctx.result("Welcome to RESTful API"));
+
+        //create connection for a server installed in localhost, with a user "root" with no password
+        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/", "root", "leahh")) {
+            // create a Statement
+            try (Statement stmt = conn.createStatement()) {
+                //execute query
+                try (ResultSet rs = stmt.executeQuery("SELECT 'Connected to mariadb!'")) {
+                    //position result to first
+                    rs.first();
+                    System.out.println(rs.getString(1)); //result is "Hello World!"
+                }
+            }
+            }
     }
 
 
