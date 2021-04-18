45f55bfb1575be80f961e24c8b380703a3777b02
Leah H <harrichl@gmail.com>
2021-04-17 21:17:06 -0400

    add client creation and not found exceptions

---
+++ src/main/java/io/lhdev/restfulapi/exceptions/ClientCreationException.java
@@ -0,0 +1,23 @@
+package io.lhdev.restfulapi.exceptions;
+
+public class ClientCreationException extends Exception{
+
+    public ClientCreationException() {
+    }
+
+    public ClientCreationException(String message) {
+        super(message);
+    }
+
+    public ClientCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ClientCreationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ClientCreationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
