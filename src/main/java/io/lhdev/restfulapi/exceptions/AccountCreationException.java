50d95bdef566163ae5d76dfe1f8e4401ebb6cea0
Leah H <harrichl@gmail.com>
2021-04-17 20:19:20 -0400

    add account creation exception

---
+++ src/main/java/io/lhdev/restfulapi/exceptions/AccountCreationException.java
@@ -0,0 +1,22 @@
+package io.lhdev.restfulapi.exceptions;
+
+public class AccountCreationException extends Exception {
+    public AccountCreationException() {
+    }
+
+    public AccountCreationException(String message) {
+        super(message);
+    }
+
+    public AccountCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccountCreationException(Throwable cause) {
+        super(cause);
+    }
+
+    public AccountCreationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
